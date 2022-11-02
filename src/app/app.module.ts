ac5a94d37663034250641bb0cf66d03c7b906752
manuel <manuel.manriquez.e@gmail.com>
2022-11-02 13:34:58 -0600

    Design only v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { ThirdComponentComponent } from './third-component/third-component.component';
+import { ThirdButtonComponent } from './third-button/third-button.component';
+import { SecondButtonComponent } from './second-button/second-button.component';
+import { FirstButtonComponent } from './first-button/first-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FirstComponentComponent,
+    SecondComponentComponent,
+    ThirdComponentComponent,
+    ThirdButtonComponent,
+    SecondButtonComponent,
+    FirstButtonComponent
   ],
   imports: [
     BrowserModule
